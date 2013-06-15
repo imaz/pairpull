2803b24dbc924b2dc8801f677a2d98f94ada5adb
IMAZU Asami <neko.daisuki.070109@gmail.com>
2013-06-16 03:21:50 +0900

    current_wish が設定されている時は設定しなおさない用に変更

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -2,8 +2,10 @@ class WishesController < ApplicationController
   def pull
     team_id = params[:team_id]
     @team = Team.find(team_id)
-    @team.current_wish = @team.wishes.where(done: false).sample
-    @team.save
+    if @team.current_wish.nil?
+      @team.current_wish = @team.wishes.where(done: false).sample
+      @team.save
+    end
     redirect_to team_path team_id
   end
 
