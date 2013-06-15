99e20488ac7a678fce0fe017003d1f35daea5e01
IMAZU Asami <neko.daisuki.070109@gmail.com>
2013-06-15 20:48:38 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pairpull::Application.config.secret_token = '7ab9ec9c804599a3aab29be38da0808b9b65efc3f0a76d4af61383353db108319830fc8c520fe5440b5bb6d93a1f14eec294c4b207c42d7ce4704dcacaab3248'
